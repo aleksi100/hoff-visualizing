0e4f12e60dfa577c5e937be72e45d5a3fe55d380
allupro <aleksi.eskola100@gmail.com>
2022-02-03 14:30:08 +0200

    now there is only on of each in NODES

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const values_div = document.querySelector('.values');
 
 let NODES = [];
 class Node{
-  constructor(letter, freq, i){
+  constructor(letter, i){
     this.letter = letter,
-    this.freq = freq,
+    this.freq = 1,
     this.id = Math.random()
     console.log(i)
     this.x = (1+i)*60
@@ -44,9 +44,19 @@ function updateValue(e) {
   }
   NODES = []
   for(let i=0;i<e.target.value.length;i++){
-    let r = new Node(e.target.value[i], 1, i)
-    r.show()
-    NODES.push(r);
+    let isFound = false
+    for(let j=0;j<NODES.length;j++){
+      if(NODES[j].letter == e.target.value[i]){
+        isFound = true
+        NODES[j].freq += 1;
+      }
+    }
+    if(!isFound){
+      let r = new Node(e.target.value[i], NODES.length)
+      r.show()
+      NODES.push(r);
+    }
+    
   }
   
 }
